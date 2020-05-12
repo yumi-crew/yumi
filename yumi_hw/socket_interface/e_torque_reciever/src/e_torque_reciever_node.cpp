46b550efa3b94403a0239ca6504e199467a48723
mariunil <nilsen-marius@hotmail.com>
2020-05-12 10:18:06 +0200

    tunign e_torque_reciever

---
+++ yumi_hw/socket_interface/e_torque_reciever/src/e_torque_reciever_node.cpp
@@ -0,0 +1,44 @@
+#include <e_torque_reciever/e_torque_reciever.hpp>
+
+std::shared_ptr<socket_interface::ETorqueReciever> etorque_reciever;
+
+// Ctr+C handler
+void signal_callback_handler(int signum)
+{
+  std::cout << "Caught signal " << signum << std::endl;
+  // Stop streams
+  etorque_reciever->stop_streams();
+  // Disconenct
+  etorque_reciever->disconnect();
+  // Terminate ros node
+  rclcpp::shutdown();
+  // Terminate script
+  exit(signum);
+}
+
+int main(int argc, char *argv[])
+{
+  // Ctrl+C handler
+  signal(SIGINT, signal_callback_handler);
+
+  rclcpp::init(argc, argv);
+  etorque_reciever = std::make_shared<socket_interface::ETorqueReciever>("e_torque_reciever", "192.168.125.1", 2021, 2020);
+
+  std::cout << "before connect()" << std::endl;
+  int num_retries = 10;
+  if (!etorque_reciever->establish_connection(num_retries))
+  {
+    std::cout << "[ERROR] connect() failed after " << num_retries << " retries." << std::endl;
+    return -1;
+  }
+
+  std::cout << "before start_streams()" << std::endl;
+  etorque_reciever->start_streams(false);
+
+
+  while (1)
+  {
+    sleep(1);
+  }
+  return 0;
+}
---
+++ yumi_test/src/demo_torque.cpp
@@ -0,0 +1,62 @@
+#include <motion_coordinator/motion_coordinator.hpp>
+
+std::shared_ptr<motion_coordinator::MotionCoordinator> yumi_motion_coordinator;
+
+
+// Ctr+C handler
+void signal_callback_handler(int signum)
+{
+  std::cout << "Caught signal " << signum << std::endl;
+  // Terminate EGM session
+  yumi_motion_coordinator->terminate_egm_session();
+  // Terminate ros node
+  rclcpp::shutdown();
+  // Terminate program
+  exit(signum);
+}
+
+void spin(std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> exe)
+{
+  exe->spin();
+}
+
+int main(int argc, char **argv)
+{
+  // Ctrl+C handler
+  signal(SIGINT, signal_callback_handler);
+
+  rclcpp::init(argc, argv);
+
+  // Initialize yumi motion coordinator
+  yumi_motion_coordinator = std::make_shared<motion_coordinator::MotionCoordinator>("onsket_nodenavn");
+  if (!yumi_motion_coordinator->init())
+    return -1;
+
+  // Spin the moveit2 node countiously in another thread via an executor
+  auto executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+  executor->add_node(yumi_motion_coordinator->get_node());
+  auto future_handle = std::async(std::launch::async, spin, executor);
+
+  // Activate yumi, EGM, Moveit2
+  if (!yumi_motion_coordinator->activate())
+  {
+    std::cout << "ERROR: MotionCoordinator failed to activate." << std::endl;
+    return -1;
+  }
+
+  
+  while (1)
+  {
+    yumi_motion_coordinator->move_to_pose("right_arm", {0.3, -0.2, 0.3, 0, 0, 180}, true, 5);
+    sleep(40);
+    yumi_motion_coordinator->move_to_pose("right_arm", {0.5, -0.2, 0.3, 0, 0, 180}, true, 5);
+    sleep(40);
+  }
+
+  std::cout << "Motion completed, please ctrl+c" << std::endl;
+  while (1)
+  {
+    sleep(1);
+  }
+  return 0;
+}
