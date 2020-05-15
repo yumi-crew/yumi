6b29210ed1e4be6ea7c24ab086e139e9c793775c
mariunil <nilsen-marius@hotmail.com>
2020-05-15 16:34:22 +0200

    dual_arm test

---
+++ yumi_test/src/demo_dual.cpp
@@ -0,0 +1,247 @@
+#include <motion_coordinator/motion_coordinator.hpp>
+#include "pose_estimation_manager.hpp"
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+std::shared_ptr<motion_coordinator::MotionCoordinator> yumi_motion_coordinator;
+std::shared_ptr<PoseEstimationManager> pose_estimation_manager;
+
+using namespace std::chrono_literals;
+
+// Ctr+C handler
+void signal_callback_handler(int signum)
+{
+  std::cout << "Caught signal " << signum << std::endl;
+  // Terminate EGM session
+  yumi_motion_coordinator->terminate_egm_session();
+  // shutdown lifecycle nodes
+  pose_estimation_manager->change_state(
+      "zivid_camera", lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE, 30s);
+  pose_estimation_manager->change_state(
+      "pose_estimation", lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE, 30s);
+  pose_estimation_manager->change_state(
+      "zivid_camera", lifecycle_msgs::msg::Transition::TRANSITION_CLEANUP, 30s);
+  pose_estimation_manager->change_state(
+      "pose_estimation", lifecycle_msgs::msg::Transition::TRANSITION_CLEANUP, 30s);
+  // Terminate ros node
+  rclcpp::shutdown();
+  // Terminate program
+  exit(signum);
+}
+
+void spin(std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> exe)
+{
+  exe->spin();
+}
+
+int main(int argc, char **argv)
+{
+  // Ctrl+C handler
+  signal(SIGINT, signal_callback_handler);
+
+  rclcpp::init(argc, argv);
+
+  // Initialize yumi motion coordinator
+  yumi_motion_coordinator = std::make_shared<motion_coordinator::MotionCoordinator>("onsket_nodenavn");
+  if (!yumi_motion_coordinator->init())
+  {
+    std::cout << "[ERROR]: MotionCoordinator failed to initialize." << std::endl;
+    return -1;
+  }
+
+  // Spin the moveit2 node countiously in another thread via an executor
+  auto executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+  executor->add_node(yumi_motion_coordinator->get_node());
+  auto future_handle = std::async(std::launch::async, spin, executor);
+
+  // Activate yumi, EGM, Moveit2
+  if (!yumi_motion_coordinator->activate())
+  {
+    std::cout << "[ERROR]: MotionCoordinator failed to activate." << std::endl;
+    return -1;
+  }
+
+  // zivid + pose_estimation
+  pose_estimation_manager = std::make_shared<PoseEstimationManager>("pose_estimation_manager");
+
+  rclcpp::executors::MultiThreadedExecutor exe;
+  exe.add_node(pose_estimation_manager);
+
+  auto state1 = pose_estimation_manager->get_state("zivid_camera", 10s);
+  auto state2 = pose_estimation_manager->get_state("pose_estimation", 10s);
+
+  auto transition_success1 = pose_estimation_manager->change_state(
+      "zivid_camera", lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE, 30s);
+  auto transition_success2 = pose_estimation_manager->change_state(
+      "pose_estimation", lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE, 30s);
+
+  pose_estimation_manager->add_camera_parameter("zivid_camera.capture.frame_0.iris", rclcpp::ParameterValue(17));
+  pose_estimation_manager->add_camera_parameter("zivid_camera.capture.frame_1.iris", rclcpp::ParameterValue(25));
+  pose_estimation_manager->add_camera_parameter("zivid_camera.capture.frame_2.iris", rclcpp::ParameterValue(38));
+  pose_estimation_manager->add_camera_parameter("zivid_camera.capture.frame_2.gain", rclcpp::ParameterValue(3.21));
+  pose_estimation_manager->add_camera_parameter("zivid_camera.capture.frame_3.iris", rclcpp::ParameterValue(42));
+  pose_estimation_manager->add_camera_parameter("zivid_camera.capture.frame_3.gain", rclcpp::ParameterValue(4.16));
+  pose_estimation_manager->add_camera_parameter("zivid_camera.capture.general.filters.reflection.enabled", rclcpp::ParameterValue(true));
+  pose_estimation_manager->call_set_param_srv(30s);
+
+  auto state3 = pose_estimation_manager->get_state("zivid_camera", 30s);
+  auto state4 = pose_estimation_manager->get_state("pose_estimation", 30s);
+
+  auto transition_success4 = pose_estimation_manager->change_state(
+      "pose_estimation", lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE, 30s);
+  auto state5 = pose_estimation_manager->get_state("zivid_camera", 30s);
+  auto state6 = pose_estimation_manager->get_state("pose_estimation", 30s);
+
+  char *buf = getlogin();
+  std::string u_name = buf;
+
+  pose_estimation_manager->call_init_halcon_surface_match_srv("/home/" + u_name + "/abb_ws/src/object_files/ply/", 500s);
+  auto transition_success3 = pose_estimation_manager->change_state(
+      "zivid_camera", lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE, 30s);
+
+
+  int ret_val;
+  int counter = 0;
+  int tries = 0;
+  int num_picks = 10;
+  bool first = true;
+  bool cap_success{false};
+  bool est_success{false};
+  std::string arm = "right_arm";
+  std::vector<std::string> objects = {"small_marker"};
+  double percentage = 0;
+  int lin_retries = 3;
+
+  // attach serving tray
+  auto gripper_pose = yumi_motion_coordinator->find_link_pose("gripper_l_center");
+  yumi_motion_coordinator->add_object("chessboard_holder", gripper_pose, false, {1, 1, 0, 1});  //yellow
+  yumi_motion_coordinator->grab_object("chessboard_holder", "left_arm");
+
+  // Move arms to setup pos.
+  yumi_motion_coordinator->move_to_home(arm, 3);
+  yumi_motion_coordinator->move_to_state("left_arm", {-0.61, -1.95,  0.94, 0.79, -1.16, 0.27, 3.29});
+  
+
+  cap_success = pose_estimation_manager->call_capture_srv(30s);
+  sleep(5);
+ 
+  // Find and add pick bin bin to scene
+  std::string pick_bin = "bin6";
+  est_success = pose_estimation_manager->call_estimate_pose_srv(pick_bin, 0, 100s, "", 0.0, true);
+  auto pick_bin_pose = pose_estimation_manager->pose_transformer->obj_in_base_frame();
+  yumi_motion_coordinator->add_object(pick_bin, pick_bin_pose, false, {0, 0, 1, 1});  //Blue
+
+  std::map<int, std::string> errors;
+  errors[0] = "SUCCESS";
+  errors[-1] = "INVALID_POSE";
+  errors[-2] = "LINEAR_PLAN_FAIL";
+  errors[-3] = "GRIP_FAIL";
+  errors[-4] = "PLANNING_SCENE_FAIL";
+  std::map<std::string, int> exp_log;
+  for (auto [key, value] : errors)
+  {
+    exp_log[value] = 0;
+  }
+  exp_log["POSE_ESTIMATION_FAIL"] = 0;
+
+  std::vector<double> cycle_times;
+  while (counter < num_picks)
+  {
+    for (auto object : objects)
+    {
+      auto cycle_start = std::chrono::high_resolution_clock::now();
+
+      tries++;
+
+      // Move away from the camera view
+      yumi_motion_coordinator->move_to_state("right_arm", {0.43, -1.32, -0.61, 0.64, -1.44, 1.27, -0.07});
+      yumi_motion_coordinator->move_to_state("left_arm", {-0.61, -1.95,  0.94, 0.79, -1.16, 0.27, 3.29});
+
+      // Take image
+      std::cout << "before call_capture_srv" << std::endl;
+      cap_success = pose_estimation_manager->call_capture_srv(30s);
+
+      // Find the boject's pose in the the camera frame
+      std::cout << "before call_estimate_pose_srv" << std::endl;
+      if (!pose_estimation_manager->call_estimate_pose_srv(object, 1, 50s, "outliers", 0.12))
+      {
+        exp_log["POSE_ESTIMATION_FAIL"]++;
+        std::cout << "[ERROR] object cannot be found." << std::endl;
+        if (yumi_motion_coordinator->object_present(object))
+          yumi_motion_coordinator->remove_object(object);
+        continue;
+      }
+
+      // Find the object's pose in the base frame of the robot
+      auto grasp_pose = pose_estimation_manager->pose_transformer->obj_in_base_frame();
+
+      // Add red mesh to detected object
+      if (!yumi_motion_coordinator->object_present(object))
+        yumi_motion_coordinator->add_object(object, grasp_pose, false, {1, 0, 0, 1});
+      else
+        yumi_motion_coordinator->move_object(object, grasp_pose);
+
+
+      // Left arm goes to serving position
+      yumi_motion_coordinator->move_to_state("left_arm", {-1.17, -1.71, 1.19, 0.37, 0.63, 1.1, 2.02}, 5, false, false);
+
+      // Pick object
+      ret_val = yumi_motion_coordinator->pick_object(arm, object, {"table", pick_bin}, lin_retries, 0.10, true, false, percentage);
+      if (ret_val < 0)
+      {
+        exp_log[errors[ret_val]]++;
+        std::cout << "pick failed" << std::endl;
+        continue;
+      }
+      
+      // Place at a object at "serving tray"
+      yumi_motion_coordinator->move_to_state("right_arm", {1.71, -0.33, -1.57, 0.74, 1.40, -1.13, 1.72}, 5);
+      yumi_motion_coordinator->drop_object(object, "right_arm");
+
+      // Right arm moves async away from camera view (same as start) (async)
+      yumi_motion_coordinator->move_to_state("right_arm", {0.43, -1.32, -0.61, 0.64, -1.44, 1.27, -0.07}, 5, false, false);
+      sleep(1);
+
+      // Left arm moves object to bin and rotates wrist.
+      yumi_motion_coordinator->move_to_state("left_arm", {-1.32, -1.7, 1.45, 0.59, 0.8, 0.74, 1.98}, 5, false, true);
+      yumi_motion_coordinator->move_to_state("left_arm", {-1.32, -1.7, 1.45, 0.59, 0.8, 0.74, 0.32}, 5, false, true);
+
+      counter++; // If place return true the object must be dropped at the destination and can thus be considered successfully picked and placed.
+      exp_log[errors[0]]++;
+      auto cycle_end = std::chrono::high_resolution_clock::now();
+      auto cycle_time = std::chrono::duration_cast<std::chrono::milliseconds>(cycle_end - cycle_start);
+      cycle_times.push_back((double)cycle_time.count()/1000.0);
+      if (counter >= num_picks)
+        break;
+    }
+  }
+
+  // go to home
+  yumi_motion_coordinator->move_to_home(arm, 3);
+
+  std::fstream log_file;
+  log_file.open("log_file.txt", std::fstream::out);
+  log_file << "Number of pics completed: " << num_picks << std::endl;
+  log_file << "Number of tries: " << tries << std::endl;
+  for (auto [error, value] : exp_log)
+  {
+    log_file << error << ": " << value << std::endl;
+  }
+  log_file << "Cycle times: ";
+  double sum{0.0};
+  for(auto ct : cycle_times)
+  {
+    log_file << ct << ", ";
+    sum += ct;
+  }
+  log_file << std::endl << "Average cycle time: " << sum / tries << std::endl;
+
+  log_file.close();
+  std::cout << "Motion completed, please ctrl+c" << std::endl;
+  while (1)
+  {
+    sleep(1);
+  }
+  return 0;
+}
---
+++ yumi_test/src/test_kdl_ik.cpp
@@ -0,0 +1,350 @@
+#include <unistd.h>
+#include <vector>
+#include <rclcpp/rclcpp.hpp>
+#include <angles/angles.h>
+#include <sensor_msgs/msg/joint_state.hpp>
+
+#include <ros2_control_interfaces/msg/joint_control.hpp>
+#include <rws_clients/grip_client.hpp>
+#include <rws_clients/robot_manager_client.hpp>
+#include <kdl_wrapper/kdl_wrapper.h>
+
+// Global joint names
+std::array<std::string, 7> joint_names_l = {
+    "yumi_joint_1_l", "yumi_joint_2_l", "yumi_joint_7_l", "yumi_joint_3_l",
+    "yumi_joint_4_l", "yumi_joint_5_l", "yumi_joint_6_l"};
+std::array<std::string, 7> joint_names_r = {
+    "yumi_joint_1_r", "yumi_joint_2_r", "yumi_joint_7_r", "yumi_joint_3_r",
+    "yumi_joint_4_r", "yumi_joint_5_r", "yumi_joint_6_r"};
+// Global storage of the latest joint states
+std::array<double, 7> recieved_joint_state_l, recieved_joint_state_r;
+KDL::JntArray recieved_joint_state_array_l, recieved_joint_state_array_r;
+// Global robot_manager
+std::shared_ptr<rws_clients::RobotManagerClient> robot_manager;
+// Global kdl_wrapper
+std::shared_ptr<KdlWrapper> kdl_wrapper;
+// Global nodes
+std::shared_ptr<rclcpp::Node> node_s_l;
+std::shared_ptr<rclcpp::Node> node_s_r;
+std::shared_ptr<rclcpp::Node> node_p_l;
+std::shared_ptr<rclcpp::Node> node_p_r;
+// Gloal publishers and subscriptions
+std::shared_ptr<rclcpp::Publisher<ros2_control_interfaces::msg::JointControl>> joint_command_publisher_r;
+std::shared_ptr<rclcpp::Publisher<ros2_control_interfaces::msg::JointControl>> joint_command_publisher_l;
+std::shared_ptr<rclcpp::Subscription<sensor_msgs::msg::JointState>> joint_state_subscription_r;
+std::shared_ptr<rclcpp::Subscription<sensor_msgs::msg::JointState>> joint_state_subscription_l;
+
+// Ctr+C handler
+void signal_callback_handler(int signum)
+{
+  std::cout << "Caught signal " << signum << std::endl;
+  // Stop EGM
+  robot_manager->stop_egm();
+  // Turn off Motors
+  robot_manager->stop_motors();
+  // Terminate ros node
+  rclcpp::shutdown();
+  // Terminate program
+  exit(signum);
+}
+
+void callback_l(sensor_msgs::msg::JointState::UniquePtr msg)
+{
+  for (int i = 0; i < 7; ++i)
+  {
+    recieved_joint_state_l[i] = msg->position[i];
+    recieved_joint_state_array_l(i) = msg->position[i];
+  }
+}
+void callback_r(sensor_msgs::msg::JointState::UniquePtr msg)
+{
+  for (int i = 0; i < 7; ++i)
+  {
+    recieved_joint_state_r[i] = msg->position[i];
+    recieved_joint_state_array_r(i) = msg->position[i];
+  }
+}
+
+// For ZYX euler angles
+KDL::Frame vec_and_euler_angles_to_frame(std::array<double, 6> pose)
+{
+  KDL::Vector vec(pose[0], pose[1], pose[2]);
+  KDL::Rotation rot = KDL::Rotation().EulerZYX(pose[3], pose[4], pose[5]);
+  return KDL::Frame(rot, vec);
+}
+
+KDL::JntArray generate_q_seed(std::string arm)
+{
+  KDL::JntArray q_seed(7);
+
+  if (arm == "r")
+  {
+    for (int i = 0; i < 7; ++i)
+    {
+      q_seed(i) = recieved_joint_state_r[i];
+      //std::cout << "q_seed " << i << " : " << q_seed(i) << std::endl;
+    }
+    return q_seed;
+  }
+  else if (arm == "l")
+  {
+    for (int i = 0; i < 7; ++i)
+    {
+      q_seed(i) = recieved_joint_state_l[i];
+      // std::cout << "q_seed " << i << " : " << q_seed(i) << std::endl;
+    }
+    return q_seed;
+  }
+  else
+  {
+    std::cout << "Error while generating q_seed, invalid arm choice" << std::endl;
+    return q_seed;
+  }
+}
+
+void generate_msg_and_publish_r(KDL::JntArray &q_config)
+{
+  ros2_control_interfaces::msg::JointControl cmd;
+  for (int i = 0; i < 7; ++i)
+  {
+    cmd.joints.push_back(joint_names_r[i]);
+    cmd.goals.push_back(q_config(i));
+  }
+  joint_command_publisher_r->publish(cmd);
+  rclcpp::spin_some(node_p_r);
+}
+
+void generate_msg_and_publish_l(KDL::JntArray &q_config)
+{
+  ros2_control_interfaces::msg::JointControl cmd;
+  for (int i = 0; i < 7; ++i)
+  {
+    cmd.joints.push_back(joint_names_l[i]);
+    cmd.goals.push_back(q_config(i));
+  }
+  joint_command_publisher_l->publish(cmd);
+  rclcpp::spin_some(node_p_l);
+}
+
+// Not used
+double sum_error(std::array<double, 7> &actual, KDL::JntArray &goal)
+{
+  double total_error = 0;
+  for (int i = 0; i < 7; ++i)
+  {
+    total_error += std::abs(actual[i] - goal(i));
+  }
+  return total_error;
+}
+
+void busy_wait_until_reached(KDL::JntArray &goal, std::string arm, double allowed_joint_error)
+{
+  if (arm == "r")
+  {
+    while (!Equal(recieved_joint_state_array_r, goal, allowed_joint_error))
+    {
+      sleep(0.004); //250hz
+    }
+    for (int i = 0; i < 7; ++i)
+    {
+      //std::cout << "Joint " << i << ", Actual: " << angles::to_degrees(recieved_joint_state_r[i])
+      // << " Goal: " << angles::to_degrees(goal(i)) << " Error: " << std::abs(angles::to_degrees(recieved_joint_state_r[i]) - angles::to_degrees(goal(i))) << std::endl;
+    }
+  }
+  else if (arm == "l")
+  {
+    while (!Equal(recieved_joint_state_array_l, goal, allowed_joint_error))
+    {
+      sleep(0.004); //250hz
+    }
+  }
+  else
+  {
+    std::cout << "Error while checking if goal is reached, invalid arm choice" << std::endl;
+  }
+}
+
+// Blocking cartesian point-to-point motion. Converts desired end-effector pose to
+// a joint configuration resulting in the pose.
+void blocking_cart_p2p_motion_right(std::array<double, 6> pose)
+{
+  KDL::Frame pose_frame = vec_and_euler_angles_to_frame(pose);
+  KDL::JntArray q_seed = generate_q_seed("r");
+  KDL::JntArray q_config = kdl_wrapper->inverse_kinematics_right(pose_frame, q_seed);
+  generate_msg_and_publish_r(q_config);
+  busy_wait_until_reached(q_config, "r", angles::from_degrees(0.01));
+}
+
+// Blocking cartesian point-to-point motion. Converts desired end-effector pose to
+// a joint configuration resulting in the pose.
+void blocking_cart_p2p_motion_left(std::array<double, 6> pose)
+{
+  KDL::Frame pose_frame = vec_and_euler_angles_to_frame(pose);
+  KDL::JntArray q_seed = generate_q_seed("l");
+  KDL::JntArray q_config = kdl_wrapper->inverse_kinematics_left(pose_frame, q_seed);
+  generate_msg_and_publish_l(q_config);
+  busy_wait_until_reached(q_config, "l", angles::from_degrees(0.01));
+}
+
+KDL::JntArray array_to_joint_array(std::array<double, 7> &array)
+{
+  KDL::JntArray jnt_array(7);
+  for (int i = 0; i < 7; ++i)
+  {
+    jnt_array(i) = angles::from_degrees(array[i]);
+  }
+  return jnt_array;
+}
+
+void go_to_home_pos()
+{
+  std::array<double, 7> goal_l = {0.0, -130.0, 135.0, 30.0, 0.0, 30.0, 0.0};
+  std::array<double, 7> goal_r = {0.0, -130.0, -135.0, 30.0, 0.0, 30.0, 0.0};
+  KDL::JntArray q_l = array_to_joint_array(goal_l);
+  KDL::JntArray q_r = array_to_joint_array(goal_r);
+  generate_msg_and_publish_l(q_l);
+  generate_msg_and_publish_r(q_r);
+  busy_wait_until_reached(q_r, "r", angles::from_degrees(0.01));
+  busy_wait_until_reached(q_l, "l", angles::from_degrees(0.01));
+}
+
+void spin(std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> exe)
+{
+  exe->spin();
+}
+
+// Joints are always given as {1,2,7,3,4,5,6}
+int main(int argc, char *argv[])
+{
+  // Ctrl+C handler
+  signal(SIGINT, signal_callback_handler);
+
+  // Setup
+  rclcpp::init(argc, argv);
+  node_s_l = rclcpp::Node::make_shared("sub_left");
+  node_s_r = rclcpp::Node::make_shared("sub_right");
+  node_p_l = rclcpp::Node::make_shared("pub_left");
+  node_p_r = rclcpp::Node::make_shared("pub_right");
+  std::string ns_l = "/l";
+  std::string ns_r = "/r";
+  recieved_joint_state_array_l.resize(7);
+  recieved_joint_state_array_r.resize(7);
+
+  // Constructing the robot_manager
+  robot_manager = std::make_shared<rws_clients::RobotManagerClient>("robot_manager_client");
+
+  // Finding path to urdf
+  char buf[20];
+  if(getlogin_r(buf, 20) != 0)
+  {
+    printf("Unable to find username\n");
+    return -1;
+  }
+  std::string username = buf;
+  std::string path = "/home/"+username+"/abb_ws/src/yumi/yumi_description/urdf/yumi.urdf";
+
+  // Loading URDf and constructing the KdlWrapper
+  urdf::Model robot_model;
+  if (!robot_model.initFile(path))
+  {
+    printf("unable to load urdf\n");
+    return -1;
+  }
+  kdl_wrapper = std::make_shared<KdlWrapper>(robot_model);
+  if (!kdl_wrapper->init())
+  {
+    printf("initialization of kdlWrapper failed\n");
+    return -1;
+  }
+
+  joint_state_subscription_l = node_s_l->create_subscription<sensor_msgs::msg::JointState>(
+      ns_l + "/joint_states",
+      10,
+      callback_l);
+  joint_command_publisher_l = node_p_l->create_publisher<ros2_control_interfaces::msg::JointControl>(
+      ns_l + "/joint_commands",
+      10);
+  joint_state_subscription_r = node_s_r->create_subscription<sensor_msgs::msg::JointState>(
+      ns_r + "/joint_states",
+      10,
+      callback_r);
+  joint_command_publisher_r = node_p_r->create_publisher<ros2_control_interfaces::msg::JointControl>(
+      ns_r + "/joint_commands",
+      10);
+
+  // Initialize the left gripper
+  auto grip_action_client_l = std::make_shared<rws_clients::GripClient>("grip_client_left", ns_l);
+  if (!grip_action_client_l->init())
+  {
+    printf("Unable to initalize the left gripper client\n");
+    return -1;
+  }
+  // Initialize the right gripper
+  auto grip_action_client_r = std::make_shared<rws_clients::GripClient>("grip_client_right", ns_r);
+  if (!grip_action_client_r->init())
+  {
+    printf("Unable to initalize the right gripper client\n");
+    return -1;
+  }
+
+  // Initialize the robot manager client
+  if (!robot_manager->init())
+  {
+    printf("Failed to initialize the robot manager client\n");
+    return -1;
+  }
+  // Blocking loop
+  while (!robot_manager->robot_is_ready())
+  {
+    sleep(0.01);
+  }
+  // Start EGM mode
+  if (!robot_manager->start_egm())
+  {
+    printf("Failed to start egm\n");
+    return -1;
+  }
+
+  // Spin nodes to recieve joint states
+  auto executor_r = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+  auto executor_l = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+  executor_r->add_node(node_s_r);
+  executor_l->add_node(node_s_l);
+  auto future_handle_r = std::async(std::launch::async, spin, executor_r);
+  auto future_handle_l = std::async(std::launch::async, spin, executor_l);
+
+  // This sleep must be larger than 2 seconds for the real robot, this should be fixed
+  sleep(5);
+
+  // Movements
+  //--------------------------------------------------------------------------------------------------------------------
+
+  // Go to home position
+  go_to_home_pos();
+
+  // Go to pose 1 {x, y, z, EZ, EY, EX}
+  std::array<double, 6> pose1 = {0.281, -0.329, 0.297,
+                                 angles::from_degrees(123), angles::from_degrees(-5), angles::from_degrees(139)};
+  blocking_cart_p2p_motion_right(pose1);
+  std::array<double, 6> pose12 = {0.300, -0.250, 0.297,
+                                 angles::from_degrees(123), angles::from_degrees(-5), angles::from_degrees(139)};
+  blocking_cart_p2p_motion_right(pose12);
+
+  // Go to pose 2 {x, y, z, EZ, EY, EX}
+  std::array<double, 6> pose2 = {0.109, 0.345, 0.286,
+                                 angles::from_degrees(-33), angles::from_degrees(-24), angles::from_degrees(-170)};
+  blocking_cart_p2p_motion_left(pose2);
+
+  // Go to home position
+  go_to_home_pos();
+
+  //--------------------------------------------------------------------------------------------------------------------
+
+  printf("motion completed, please ctrl+c\n");
+  while (1)
+  {
+    sleep(1);
+  }
+
+  return 0;
+}
