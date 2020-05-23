d4117a55b3f06a2208aa842547b188a8de8ccaaf
Marius Nilsen <nilsen-marius@hoymail.com>
2020-05-23 14:04:28 +0200

    trajectory controller formatting

--- yumi_control/controllers/include/controllers/joint_trajectory_controller.hpp
+++ yumi_control/controllers/include/controllers/joint_trajectory_controller.hpp
@@ -1,10 +1,22 @@
-#ifndef ROS_CONTROLLERS__JOINT_TRAJECTORY_CONTROLLER_HPP_
-#define ROS_CONTROLLERS__JOINT_TRAJECTORY_CONTROLLER_HPP_
+// Copyright 2020 Norwegian University of Science and Technology.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
 
 #include <memory>
 #include <string>
 #include <vector>
-
 #include "controller_interface/controller_interface.hpp"
 #include "hardware_interface/operation_mode_handle.hpp"
 #include "hardware_interface/robot_hardware.hpp"
@@ -16,14 +28,12 @@
 #include <std_msgs/msg/bool.hpp>
 
 
-
 namespace ros_controllers
 {
 
 class JointTrajectoryController : public controller_interface::ControllerInterface
 {
 public:
-
   ROS_CONTROLLERS_PUBLIC
   JointTrajectoryController();
 
@@ -64,7 +74,6 @@ public:
   on_shutdown(const rclcpp_lifecycle::State & previous_state) override;
 
 private:
-
   std::vector<std::string> joint_names_;
   std::vector<std::string> write_op_names_;
 
@@ -93,5 +102,3 @@ private:
 };
 
 }  // namespace ros_controllers
-
-#endif  // ROS_CONTROLLERS__JOINT_TRAJECTORY_CONTROLLER_HPP_
